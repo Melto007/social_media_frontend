81c2ea2c0826eb1bb529893f981b67e3dd1e8f7c
Melto007 <meltosm8@gmail.com>
2024-02-11 10:15:18 +0530

    changes in redux store

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,5 +1,4 @@
-import { legacy_createStore as createStore, combineReducers, applyMiddleware } from 'redux'
-import { thunk } from 'redux-thunk'
+import { configureStore } from '@reduxjs/toolkit'
 
 import {
     registerReducer,
@@ -9,16 +8,14 @@ import {
     resetPasswordReducer
 } from '../reducers/registerReducers'
 
-const reducer = combineReducers({
-    register: registerReducer,
-    login: loginReducer,
-    userReducer: userReducer,
-    forgotpasswordReducer: forgotpasswordReducer,
-    resetPasswordReducer: resetPasswordReducer
+const store = configureStore({
+    reducer: {
+        register: registerReducer,
+        login: loginReducer,
+        userReducer: userReducer,
+        forgotpasswordReducer: forgotpasswordReducer,
+        resetPasswordReducer: resetPasswordReducer
+    }
 })
-
-const middleware = [thunk]
-
-const store = createStore(reducer, applyMiddleware(...middleware))
 
 export default store
